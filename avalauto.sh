d2c012bc130778a22ec5a6c72e38c65395eb9f57
Acktarius <94026250+Acktarius@users.noreply.github.com>
2024-01-04 21:03:29 +0000

    Update avalauto.sh

--- avalauto.sh
+++ avalauto.sh
@@ -5,6 +5,9 @@
 #    main
 ##################################################################
 #variables
+declare -A av=(["hash"]="?" ["status"]="?" ["power"]="?" ["tavg"]="?" ["pool"]="?" ["worker"]="?" ["accepted"]="?" ["rejected"]="?" ["rejectedp"]="?")
+##index
+declare -a i
 bold=$(tput bold)
 normal=$(tput sgr0)
 if [[ ! -f .data ]]; then
@@ -43,6 +46,7 @@ if ([[ -n $1 ]] && [[ $1 = "--clearlog" ]]); then
 if [[ -f hashlog.txt ]]; then
 	echo "CURRENTDATE hashrate: ? TH/s at ? Watt, ? W/TH/s, Status ? Tavg ? TIMESTAMP" > hashlog.txt
 	echo "log as been cleared"; sleep 2; 
+
 else
 echo "no log" 
 fi
@@ -58,8 +62,8 @@ curlit
 unset -f curlit
 sleep 2
 
-#run for 5 minutes
-for ((t=0; t <10; t++)); do
+#run for 2 minutes
+for ((t=0; t <4; t++)); do
 
 #Get Hash
 hashbrut=$(curl -s -X GET --cookie-jar cookies.txt \
@@ -67,11 +71,21 @@ hashbrut=$(curl -s -X GET --cookie-jar cookies.txt \
 	--config curlConfigFile.txt \
 	--digest \
 	$url2 \
-	| grep '"av":')
-i=$(strindex "$hashbrut" '"av":"')
-l=$(strindex "$hashbrut" '"sys_status":')
-hash="${hashbrut:(($i+6)):2}"
-status="${hashbrut:(($l+14)):1}"
+	| grep '"av":')	
+i[0]=$(strindex "$hashbrut" '"av":')
+i[1]=$(strindex "$hashbrut" '"sys_status":')
+i[2]=$(strindex "$hashbrut" '"url":')
+i[3]=$(strindex "$hashbrut" '"worker":')
+i[4]=$(strindex "$hashbrut" '"accepted":')
+i[5]=$(strindex "$hashbrut" '"reject":')
+i[6]=$(strindex "$hashbrut" '"rejected_percentage":')
+av["hash"]=$(echo "${hashbrut:((${i[0]}+6)):10}" | cut -d '"' -f 1 | cut -d "." -f 1 )
+av["status"]="${hashbrut:((${i[1]}+14)):1}"
+av["pool"]=$(echo "${hashbrut:((${i[2]}+7)):40}" | cut -d '"' -f 1 )
+av["worker"]=$(echo "${hashbrut:((${i[3]}+10)):20}" | cut -d '"' -f 1)
+av["accepted"]=$(echo "${hashbrut:((${i[4]}+12)):10}" | cut -d '"' -f 1)
+av["rejected"]=$(echo "${hashbrut:((${i[5]}+10)):10}" | cut -d '"' -f 1)
+av["rejectedp"]=$(echo "${hashbrut:((${i[6]}+23)):10}" | cut -d '"' -f 1)
 unset i l
 
 #Get Power
@@ -81,16 +95,19 @@ powerbrut=$(curl -s -X GET --cookie-jar cookies.txt \
 	--digest \
 	$url3 \
 	)
-j=$(strindex "$powerbrut" 'PS[')
-k=$(strindex "$powerbrut" 'TAvg[')
-power="$(echo "${powerbrut:(($j+3)):20}" | cut -d " " -f 5)"
-tavg="${powerbrut:(($k+5)):2}"
-unset j k
+i[7]=$(strindex "$powerbrut" 'PS[')
+i[8]=$(strindex "$powerbrut" 'TAvg[')
+av["power"]="$(echo "${powerbrut:((${i[7]}+3)):20}" | cut -d " " -f 5)"
+av["tavg"]=$(echo "${powerbrut:((${i[8]}+5)):5}" | cut -d ']' -f 1)
+
 
 
 CURRENTDATE=$(date +"%Y-%m-%d %T")
 TIMESTAMP=$(date +%s)
-echo "$CURRENTDATE hashrate: $hash TH/s at $power Watt, $(( $power / $hash )) W/TH/s, Status $status Tavg $tavg $TIMESTAMP" >> hashlog.txt
+echo "$CURRENTDATE hashrate: ${av["hash"]} TH/s at ${av["power"]} Watt, $(( ${av["power"]} / ${av["hash"]} )) W/TH/s, \
+	Status ${av["status"]} Tavg ${av["tavg"]} $TIMESTAMP \
+${av["pool"]} ${av["worker"]} ${av["accepted"]} ${av["rejected"]} ${av["rejectedp"]}" \
+>> hashlog.txt
 
 sleep 30
 
@@ -103,3 +120,6 @@ curl -X GET --cookie-jar cookies.txt \
 	--digest \
 	$urlq \
 	2> /dev/null
+
+unset i av
+
