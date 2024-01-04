872e5576b73d73dc67d41e005a83e6fd720be82b
Acktarius <94026250+Acktarius@users.noreply.github.com>
2024-01-04 00:53:59 +0000

    Update avalon_ext.3r.sh

--- avalon_ext.3r.sh
+++ avalon_ext.3r.sh
@@ -11,7 +11,7 @@ hash=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 4)
 watt=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 7)
 ratio=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 9)
 status=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 12)
-tmax=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 14)
+tavg=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 14)
 laststamp=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 15)
 
 now=$(date +%s)
@@ -32,6 +32,6 @@ echo "---"
 echo "on <span color='$(color $delta "3600")'>$lastDay</span> at <span color='$(color $delta "3600")'>$lastClock</span> | size=10"
 echo "Hash <span color='#1fc600'>${hash}</span> TH/s at <span color='#1fc600'>${watt}</span> Watt"
 echo "Status <span color='$(color "0" $status)'>$status</span> Ratio: <span color='#1fc600'>${ratio}</span> W/H/s"
-echo "Temp max <span color='$(color $tmax 72)'>$tmax</span>"
+echo "Temp avg <span color='$(color $tavg 70)'>$tavg</span>"
 echo ":hammer_and_pick:  Run Script | bash=/opt/avalauto/launcher.sh terminal=false"
 echo "empty :wastebasket: log | bash=/opt/avalauto/trashlog.sh terminal=false"
