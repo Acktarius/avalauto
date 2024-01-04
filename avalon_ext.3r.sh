f76b02fbcc2d837774e6fb9dfd23bf1a4854b12b
Acktarius <94026250+Acktarius@users.noreply.github.com>
2024-01-04 21:05:20 +0000

    Update avalon_ext.3r.sh

--- avalon_ext.3r.sh
+++ avalon_ext.3r.sh
@@ -4,15 +4,23 @@
 ##################################################################
 #  argos extemsion script
 ##################################################################
-#variables
-lastDay=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 1)
-lastClock=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 2)
-hash=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 4)
-watt=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 7)
-ratio=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 9)
-status=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 12)
-tavg=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 14)
-laststamp=$(tail -n 1 /opt/avalauto/hashlog.txt | cut -d " " -f 15)
+#variables and declarations
+declare -a lastLine
+declare -a poolInfo
+read -a lastLine <<< $(echo "$(tail -n 1 /opt/avalauto/hashlog.txt)")
+lastDay=${lastLine[0]}
+lastClock=${lastLine[2]}
+hash=${lastLine[3]}
+watt=${lastLine[6]}
+ratio=${lastLine[8]}
+status=${lastLine[11]}
+tavg=${lastLine[13]}
+laststamp=${lastLine[14]}
+
+for ((i=15; i<20; i++)); do
+poolInfo[$(($i-15))]=${lastLine[$i]}
+done
+
 
 now=$(date +%s)
 delta=$(( $now - $laststamp  ))
@@ -29,9 +37,17 @@ fi
 #extension
 echo "Avalon | iconName=miner"
 echo "---"
-echo "on <span color='$(color $delta "3600")'>$lastDay</span> at <span color='$(color $delta "3600")'>$lastClock</span> | size=10"
+echo "on <span color='$(color $delta "1800")'>$lastDay</span> at <span color='$(color $delta "1800")'>$lastClock</span> | size=10"
 echo "Hash <span color='#1fc600'>${hash}</span> TH/s at <span color='#1fc600'>${watt}</span> Watt"
 echo "Status <span color='$(color "0" $status)'>$status</span> Ratio: <span color='#1fc600'>${ratio}</span> W/H/s"
 echo "Temp avg <span color='$(color $tavg 70)'>$tavg</span>"
+echo "Pool info"
+echo "--${poolInfo[0]} | size=9"
+echo "--worker: ${poolInfo[1]} | size=9"
+echo "--accepted share: ${poolInfo[2]} | size=9"
+echo "--rejected share: ${poolInfo[3]}    <span color='$(color $(printf '%.0f\n' ${poolInfo[4]}) 2)'>${poolInfo[4]}%</span> | size=9"
+echo "---"
 echo ":hammer_and_pick:  Run Script | bash=/opt/avalauto/launcher.sh terminal=false"
-echo "empty :wastebasket: log and :hammer_and_pick: Run| bash=/opt/avalauto/trashlog.sh terminal=false"
+echo "empty :wastebasket: log and :hammer_and_pick: Run | bash=/opt/avalauto/trashlog.sh terminal=false"
+
+unset lastLine poolInfo
