9d0dc524c551bc93a06dc8c911773399763e4dff
Acktarius <94026250+Acktarius@users.noreply.github.com>
2024-01-05 21:44:20 +0000

    post method

---
+++ miner_opsR.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# this file is subject to Licence
+#Copyright (c) 2024, Acktarius
+##################################################################
+
+wdir="/opt/avalauto"
+cd $wdir
+
+source /opt/avalauto/miner_ops.sh R
+
+exit
